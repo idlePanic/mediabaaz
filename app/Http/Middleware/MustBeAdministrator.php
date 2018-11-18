760ccaaaf0d755ccd576d70eec0228497655fa8c
idlePanic <hamidpahlevani1994@gmail.com>
2018-11-18 19:06:25 +0330

    midlewares project

---
+++ app/Http/Middleware/MustBeAdministrator.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class MustBeAdministrator
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if(\Auth::user() && \Auth::user()->is_admin ==1){
+            return $next($request);
+        }
+        return redirect('/');
+    }
+}
